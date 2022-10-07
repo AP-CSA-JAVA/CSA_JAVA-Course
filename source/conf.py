171d3b6f071f65130d0498c2c7b30bc770528460
jonvirak <jonvirak@users.noreply.github.com>
2022-10-07 13:28:51 -0700

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -51,7 +51,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'furo'
+html_theme = 'sphinx_rtd_theme'
 
 html_title = project
 
