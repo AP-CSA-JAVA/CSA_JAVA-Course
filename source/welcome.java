7253ceefcecc2d445707de843268142b1f856c64
Jovi <jonvirak@users.noreply.github.com>
2024-03-11 09:57:03 -0700

    Update welcome.java

--- source/welcome.java
+++ source/welcome.java
@@ -1,7 +1,7 @@
 package source;
 class Main {
   public static void main(String[] args) {
-    System.out.println("\n*** Mr. Virak and Yoda welcome \nyou to the world of Java ***");
+    System.out.println("\n*** Yoda & I welcome \nyou to the world of Java ***");
 		System.out.println();
 		System.out.println("       Java Learn can I!");
 		System.out.println(" (\\__/)");
