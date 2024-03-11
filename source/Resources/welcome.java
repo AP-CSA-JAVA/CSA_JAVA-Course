422bcf34432ffad01c78907d98d7498287234238
jonvirak <jonvirak@users.noreply.github.com>
2024-03-11 10:10:45 -0700

    Create welcome.java

---
+++ source/Resources/welcome.java
@@ -0,0 +1,11 @@
+package source;
+class Main {
+  public static void main(String[] args) {
+    System.out.println("\n*** Yoda & I welcome \nyou to the world of Java ***");
+		System.out.println();
+		System.out.println("       Java Learn can I!");
+		System.out.println(" (\\__/)");
+		System.out.println(" (o^.^)");
+		System.out.println("z(_(\")(\")");
+  }
+}
