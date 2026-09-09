18929d45fd60231296b2fd49a04d657f44f85c77
Jovi <jonvirak@users.noreply.github.com>
2026-09-09 11:58:00 -0700

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -31,11 +31,13 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'myst_parser'
+    'myst_parser',
+    'sphinx.ext.mathjax'
 ]
 
 myst_enable_extensions = [
-    'tasklist'
+    'tasklist',
+    'dollarmath'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
